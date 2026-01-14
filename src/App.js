1ce076b090f5b5ff24e16baf816aa1f165c10cc0
sol3citoo <devarodriguezgomez@gmail.com>
2026-01-14 13:45:39 +0100

    Creación base de la app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          APLICACIÓN DE INCIDENCIAS - GRUPO 3
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
